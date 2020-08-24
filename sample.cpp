57d12a12c2a235f84e83e3708145f6f976edd913
Darwin Xu <darwin.xu@icloud.com>
2020-08-24 21:42:43 +0800

    Some adjustment

---
+++ sample.cpp
@@ -0,0 +1,93 @@
+class AAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
+    : Base1
+    , Base2
+{
+public:
+    AAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBCCCCCCCCCCCCCCCCCCCCCCCCCCCCC()
+        : aaaaaaaaaa(1)
+        , bbbbbbbbbb(1)
+        , cccccccccc(1)
+        , dddddddddd(1)
+    {
+    }
+
+    AAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBCCCCCCCCCCCCCCCCCCCCCCCCCCCCC()
+        : aaaaaaaaaa(1)
+    {
+    }
+
+private:
+    int aaaaaaaaaa;
+    int bbbbbbbbbb;
+    int cccccccccc;
+    int dddddddddd;
+};
+
+class SomeClass
+{
+public:
+    SomeClass();
+};
+
+SomeClass::SomeClass()
+    : aaaaaaaa(aaaaaaaa)
+{
+    return 0;
+}
+
+void myFun1(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j);
+
+void myFun2(int aaaaaaaa,
+            int bbbbbbbb,
+            int cccccccc,
+            int dddddddd,
+            int eeeeeeee,
+            int ffffffff,
+            int gggggggg,
+            int hhhhhh);
+
+void myFun3(int aaaaaaaa,
+            int bbbbbbbb,
+            int cccccccc,
+            int dddddddd,
+            int eeeeeeee,
+            int ffffffff,
+            int gggggggg,
+            int hhhhhhhh,
+            int iiiiiiii,
+            int jjjjjjjj);
+
+void func()
+{
+    callFunctionnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn(111111,
+                                                                           111111,
+                                                                           222222,
+                                                                           222222,
+                                                                           333333,
+                                                                           444444);
+
+    int n;
+    switch (n)
+    {
+    case 1:
+        break;
+    case 2:
+        break;
+    }
+}
+
+char* aaaa = "bbbb"
+             "cccc";
+
+extern "C" {
+int foo();
+}
+
+class Foo : Bar
+{
+}
+
+var arr = [1, 2, 3];
+f({a: 1, b: 2, c: 3});
+
+// haha
